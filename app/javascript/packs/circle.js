ba4069f3a053d28b11d35751ae74c5b2560cbf23
Shenaj Marla mshenaj@gmail.com <mshenaj@gmail.com>
2020-04-28 16:45:36 +0200

    circle home - done

---
+++ app/javascript/packs/circle.js
@@ -0,0 +1,6 @@
+// const drawCircle = document.getElementById('draw')
+// drawCircle.addEventListener ('click', (e) => {
+//   e.transform('rotate(120deg)');
+//   e.transform('rotate(-120deg)');
+// })
+
